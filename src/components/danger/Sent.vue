84e4f6c0984c31629e9123d1222228a8d490c215
tomcifa <t.cifariellociardi@studenti.unitn.it>
2025-02-11 01:43:37 +0100

    added sent dangers

---
+++ src/components/danger/Sent.vue
@@ -0,0 +1,31 @@
+<script setup>
+import EmptyList from './EmptyList.vue'
+import List from './List.vue'
+import UserActionButtons from './UserActionButtons.vue'
+import { ref } from 'vue'
+import { loggedUser } from '@/states/loggedUser'
+import { bus } from 'vue3-eventbus'
+
+let dangers = ref([])
+
+async function fetchDangers() {
+  await fetch(import.meta.env.VITE_API_ENDPOINT + '/dangers/?sentBy=' + loggedUser._id, {
+    method: 'GET'
+  })
+  .then((resp) => resp.json())
+  .then((data) => {
+    dangers.value = data
+  })
+}
+
+await fetchDangers()
+
+bus.on('userActionEnd', (e) => fetchDangers())
+</script>
+
+<template>
+  <div class="tab-content mx-auto container-sm my-4" style="max-width: 1000px">
+    <List v-if="dangers.length > 0" :dangers="dangers" :buttons="UserActionButtons" />
+    <EmptyList v-else subheader="Aggiungi una segnalazione per iniziare" />
+  </div>
+</template>
---
+++ src/views/sentDangers.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Sent from '@/components/danger/Sent.vue';
+import HeaderBar from '@/components/HeaderBar.vue';
+
+</script>
+
+<template>
+  <HeaderBar header="Le mie segnalazioni" subheader="Visualizza le segnalazioni di pericolo che hai inviato"/>
+    <Suspense>
+      <Sent/>
+
+      <template #fallback>
+      </template>
+    </Suspense>
+</template>
